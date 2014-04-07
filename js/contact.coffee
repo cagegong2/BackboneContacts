bc405b892ce7bce097ad4e200ad4ac47c1cbdbfb
cagegong <cagegong@163.com>
2014-04-07 22:28:40 +0800

    add view

--- js/contact.coffee
+++ js/contact.coffee
@@ -10,6 +10,53 @@ contact1 = new Contact
 
 console.log contact1.url()
 
+contact2 = new Contact
+	name:'tom'
+	id:2
+
+contact3 = new Contact
+	name:'jerry'
+	id:3
+
+contact4 = new Contact
+	name:'alex'
+	id:4
+
+ContactBook = Backbone.Collection.extend
+	model: Contact
+
+contacts = new ContactBook([contact1, contact2, contact3, contact4])
+
+contacts.remove(contact4)
+
+console.log contacts.length
+
+ContactView = Backbone.View.extend
+	tagName:'li'
+	initialize: ->
+		this.listenTo(this.model,'change',this.render)
+	render: ->
+		this.$el.html('<div><h3>'+this.model.attributes.name+'</h3><p>'+this.model.attributes.description+'</p></div>')
+		return this
+
+AppView = Backbone.View.extend
+	el: '#main'
+	initialize: ->
+		this.$list = $('#contacts')
+		this.addAll()
+	addOne: (contact)->
+		view = new ContactView
+			model: contact
+		this.$list.append(view.render().el)
+
+	addAll: ->
+		this.$list.html('')
+		contacts.each(this.addOne, this)
+
+	render: ->
+
+new AppView()
+
 Workspace = Backbone.Router.extend
 	routes:
 		"help": "help"
--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,4 @@
-var Contact, Workspace, app, contact1;
+var AppView, Contact, ContactBook, ContactView, Workspace, app, contact1, contact2, contact3, contact4, contacts;
 
 Contact = Backbone.Model.extend({
   urlRoot: 'http://coffeescript.org/',
@@ -16,6 +16,64 @@ contact1 = new Contact({
 });
 
 console.log(contact1.url());
+
+contact2 = new Contact({
+  name: 'tom',
+  id: 2
+});
+
+contact3 = new Contact({
+  name: 'jerry',
+  id: 3
+});
+
+contact4 = new Contact({
+  name: 'alex',
+  id: 4
+});
+
+ContactBook = Backbone.Collection.extend({
+  model: Contact
+});
+
+contacts = new ContactBook([contact1, contact2, contact3, contact4]);
+
+contacts.remove(contact4);
+
+console.log(contacts.length);
+
+ContactView = Backbone.View.extend({
+  tagName: 'li',
+  initialize: function() {
+    return this.listenTo(this.model, 'change', this.render);
+  },
+  render: function() {
+    this.$el.html('<div><h3>' + this.model.attributes.name + '</h3><p>' + this.model.attributes.description + '</p></div>');
+    return this;
+  }
+});
+
+AppView = Backbone.View.extend({
+  el: '#main',
+  initialize: function() {
+    this.$list = $('#contacts');
+    return this.addAll();
+  },
+  addOne: function(contact) {
+    var view;
+    view = new ContactView({
+      model: contact
+    });
+    return this.$list.append(view.render().el);
+  },
+  addAll: function() {
+    this.$list.html('');
+    return contacts.each(this.addOne, this);
+  },
+  render: function() {}
+});
+
+new AppView();
 
 Workspace = Backbone.Router.extend({
   routes: {
